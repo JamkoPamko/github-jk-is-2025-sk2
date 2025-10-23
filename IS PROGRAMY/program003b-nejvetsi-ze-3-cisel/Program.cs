4a087979671d3e7a235f01b30c1b13e2a6b1b92c
JamkoPamko <Kuba.Kubovy@seznam.cz>
2025-10-23 15:06:44 +0200

    #6 vybrani nej cisla ze 3

---
+++ IS PROGRAMY/program003b-nejvetsi-ze-3-cisel/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics;
+using System.Runtime.ExceptionServices;
+
+string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************************************");
+    Console.WriteLine("*********** Výpis číselné řady *************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************* Jakub Kubový *****************");
+    Console.WriteLine("************** 13.10.2025 ******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+
+
+    Console.Write("Zadejte celé čílo A: ");
+    int a;
+    while (!int.TryParse(Console.ReadLine(), out a))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte celé čílo znovu A: ");
+    }
+
+    Console.Write("Zadejte celé číslo B: ");
+    int b;
+    while (!int.TryParse(Console.ReadLine(), out b))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte celé číslo snovu B: ");
+    }
+
+   Console.Write("Zadejte celé číslo C: ");
+    int c;
+    while (!int.TryParse(Console.ReadLine(), out c))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte celé číslo snovu C: ");
+    }
+    Console.WriteLine();
+
+    if (a > b)
+    {
+        if (a > c)
+            Console.WriteLine($"Největší číslo je A = {a}");
+        else
+            Console.WriteLine($"Největší číslo je C = {c}");
+    }
+    else
+    {
+        if (b > c)
+            Console.WriteLine($"Největší číslo je B = {b}");
+        else
+            Console.WriteLine($"Největší číslo je C = {c}");
+
+    }
+
+
+
+
+        Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+
+}
