892ea985e5d3f416b9f8e8dd238e1e6af3cb03c0
JamkoPamko <Kuba.Kubovy@seznam.cz>
2025-12-30 15:05:34 +0100

    #7 obrazec 9 Z

---
+++ IS PROGRAMY/program004d-obrazec9/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+using System.Runtime.ExceptionServices;
+
+string again = "a";
+while (again == "a")
+{
+    //Console.Clear();
+    Console.WriteLine("********************************************");
+    Console.WriteLine("*********** Výpis číselné řady *************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************* Jakub Kubový *****************");
+    Console.WriteLine("************** 13.10.2025 ******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+
+    Console.Write("Zadejte první číslo řady (celé číslo): ");
+    int first;
+    while (!int.TryParse(Console.ReadLine(), out first))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
+    }
+
+    for (int radek = 0; radek < first; radek++)
+    {
+        for (int sloupec = 0; sloupec < first; sloupec++)
+        {
+            int mezera = 2;         // velikost jedné buňky
+            int krok = mezera + 1;  // perioda mřížky
+
+            if ()
+
+
+
+
+
+        }
+        
+        
+        
+        
+        Console.WriteLine();
+    }
+
+
+
+
+ Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+
+}
---
+++ IS PROGRAMY/program004e-obrazec4/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Runtime.ExceptionServices;
+using Microsoft.VisualBasic;
+
+string again = "a";
+while (again == "a")
+{
+    //Console.Clear();
+    Console.WriteLine("********************************************");
+    Console.WriteLine("*********** Výpis číselné řady *************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************* Jakub Kubový *****************");
+    Console.WriteLine("************** 13.10.2025 ******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+
+    Console.Write("Zadejte první číslo řady (celé číslo): ");
+    int first;
+    while (!int.TryParse(Console.ReadLine(), out first))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
+    }
+
+ for (int radek = 0; radek < first; radek++)
+    {
+        for (int sloupec = 0; sloupec < first; sloupec++)
+        {
+            //příkaz pro udělání Z
+            if (radek == first - 1 || radek == 0 || radek + sloupec == first -1)
+            {
+                Console.Write("* ");
+            }
+            else
+            {
+                Console.Write("  ");
+            }
+
+
+
+
+
+        }
+        
+        
+        
+        
+        Console.WriteLine();
+    }
+
+
+
+
+
+ Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+
+}
