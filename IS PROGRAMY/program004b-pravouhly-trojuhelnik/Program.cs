66e9ef6b37c21dfb2bf4a0543513fc2d861b672e
JamkoPamko <Kuba.Kubovy@seznam.cz>
2025-12-30 12:35:25 +0100

    #6 pravouhly trojuhelnik

---
+++ IS PROGRAMY/program004b-pravouhly-trojuhelnik/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using System.Runtime.ExceptionServices;
+
+string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************************************");
+    Console.WriteLine("*********** pravoúhlý trojuhelník *************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************* Jakub Kubový *****************");
+    Console.WriteLine("**************  ******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+
+    Console.Write("Zadejte první číslo řady (celé číslo): ");
+    int first;
+    while (!int.TryParse(Console.ReadLine(), out first))
+    {        
+        Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
+    }
+
+    for (int i = 0; i < first; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            Console.Write("* ");
+        }   
+               
+        
+        Console.WriteLine();
+    }
+
+
+
+
+
+ Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+
+}
